299514ca93cc1b60a8edcaca9024eb242b5f8ad2
Léo GRAND <leograndcontact@gmail.com>
2018-06-28 13:11:53 +0200

    initial commit

---
+++ resume.js
@@ -0,0 +1,6 @@
+import 'bulma';
+import './resume.css';
+
+document.addEventListener("DOMContentLoaded", function() {
+    let toggleable = document.getElementsByClassName('toggleable');                
+});
