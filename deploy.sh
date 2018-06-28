63dcf107887b9d11836f4a13c4cd57fab0566832
Léo GRAND <leograndcontact@gmail.com>
2018-06-28 14:10:57 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DIR=$(PWD)
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+
+echo "Starting deploy..."
+mkdir /tmp/deploy-github && \
+cd /tmp/deploy-github && \
+git clone -b gh-pages https://github.com/MUCHZER/about.git && \
+git rm -f * && \
+cp $DIR/dist/* . && \
+git add * && \
+git commit -m "build at : $DATE" && \
+git push && \
+rm -rf /tmp/deploy-github && \
+echo "Deployed at $DATE !"
+
