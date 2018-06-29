749049a2a1948b53a0a90c198b816db92ee4990b
GRAND Léo <leograndcontact@gmail.com>
2018-06-29 03:05:56 +0200

    Updated resume

--- resume.js
+++ resume.js
@@ -1,6 +1,14 @@
 import 'bulma';
 import './resume.css';
 
+console.log('Nothing to see here :)');
+
 document.addEventListener("DOMContentLoaded", function() {
     let toggleable = document.getElementsByClassName('toggleable');                
+    window.easteregg = function() {
+        for(var i=0, len=toggleable.length; i<len; i++)
+        {
+            toggleable[i].style["display"] = 'block';
+        }
+    }
 });
